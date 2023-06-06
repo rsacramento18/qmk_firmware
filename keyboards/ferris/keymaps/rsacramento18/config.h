e507f02b0ad8ce873a68084d628ba93cb1fa6a51
Ricardo Sacramento <sacramentor@webeo.com>
2023-06-06 20:39:27 +0100

    Adding serial driver to rsacramento18 ferris sweep keymap

--- keyboards/ferris/keymaps/rsacramento18/config.h
+++ keyboards/ferris/keymaps/rsacramento18/config.h
@@ -43,10 +43,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED D5 // Specify an optional status LED by GPIO number which blinks when entering the bootloader
-                                                  //
-#define MASTER_LEFT
 
-// Underglow configuration
+#define SPLIT_USB_DETECT
+#define SPLIT_USB_TIMEOUT 2500
+
 #ifdef RGBLIGHT_ENABLE
 #    define RGBLIGHT_EFFECT_BREATHING
 #    define RGBLIGHT_EFFECT_RAINBOW_MOOD
--- keyboards/ferris/keymaps/rsacramento18/rules.mk
+++ keyboards/ferris/keymaps/rsacramento18/rules.mk
@@ -1,2 +1,3 @@
 CONVERT_TO = promicro_rp2040
 SPLIT_KEYBOARD = yes
+BOOTLOADER = rp2040
